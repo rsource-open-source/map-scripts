9b5cbfa375a4599a12584e6c810842538828fdfb
insyri <insyri@users.noreply.github.com>
2021-10-21 02:16:02 +0000

    CI: Prettier Formatting

--- maps/{test}/src/script.ts
+++ maps/{test}/src/script.ts
@@ -1,7 +1,9 @@
 import { CollectionService } from "@rbxts/services";
 
 for (const obj of CollectionService.GetTagged("Lava")) {
-	if (obj.IsA("BasePart")) {
-		obj.Touched.Connect(part => part.Parent?.FindFirstChildOfClass("Humanoid")?.TakeDamage(100));
-	}
+  if (obj.IsA("BasePart")) {
+    obj.Touched.Connect((part) =>
+      part.Parent?.FindFirstChildOfClass("Humanoid")?.TakeDamage(100)
+    );
+  }
 }
